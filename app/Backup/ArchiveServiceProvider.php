0137a4211299414c1a52d52990f175c96b00d1a4
Pavel Petrov <ppu@itsit.org>
2017-04-28 16:09:47 +0300

    translation file + translation reslover in ArchiveServiceProvider boot method

--- app/Backup/ArchiveServiceProvider.php
+++ app/Backup/ArchiveServiceProvider.php
@@ -38,5 +38,24 @@ extends ServiceProvider
     public function boot(ClassMap $classMap)
     {
         $classMap->map('pkg.backup.archive', Archive::class);
+
+        $this->loadTranslationsFrom(
+            $this->basePath().'/resources/lang',
+            'pkg.'.$this->folder()
+        );
+    }
+
+    protected function folder()
+    {
+        return 'backup';
+    }
+
+    protected function basePath()
+    {
+        return sprintf(
+            '%s/packages/%s',
+            $this->app->basePath(),
+            $this->folder()
+        );
     }
 }
