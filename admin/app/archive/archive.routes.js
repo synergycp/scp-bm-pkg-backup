f63c46b6209f8a7096ff06383806c1932d162bb0
Zane Hooper <zanehoop@gmail.com>
2017-05-01 14:38:31 -0400

    Log URL

--- admin/app/archive/archive.routes.js
+++ admin/app/archive/archive.routes.js
@@ -1,32 +1,33 @@
 (function () {
-    angular
-        .module('pkg.backup.archive')
-        .config(routeConfig)
+  angular
+    .module('pkg.backup.archive')
+    .config(routeConfig)
+  ;
+
+  /**
+   * @ngInject
+   */
+  function routeConfig(RouteHelpersProvider) {
+    var helper = RouteHelpersProvider;
+    var pkg = helper.package('backup');
+    pkg
+      .state('archive', {
+        url: '/archive',
+        abstract: true,
+        template: helper.dummyTemplate,
+        resolve: helper.resolveFor(pkg.lang('admin:archive')),
+      })
+      .url('archive/?([0-9]*)', mapArchiveUrl)
+      .sso('archive', function ($state, options) {
+        return mapArchiveUrl($state, options.id);
+      })
     ;
 
-    /**
-     * @ngInject
-     */
-    function routeConfig(RouteHelpersProvider) {
-        var helper = RouteHelpersProvider;
-        var pkg = helper.package('backup');
-        pkg
-            .state('archive', {
-                url: '/archive',
-                abstract: true,
-                template: helper.dummyTemplate,
-                resolve: helper.resolveFor(pkg.lang('admin:archive')),
-            })
-            .url('archive/?([0-9]*)', mapArchiveUrl)
-            .sso('archive', function($state, options) {
-                return mapArchiveUrl($state, options.id);
-            })
-        ;
-
-        function mapArchiveUrl($state, id) {
-            return $state.href('archive.' + (id ? 'view' : 'list'), {
-                id: id,
-            });
-        }
+    function mapArchiveUrl($state, id) {
+      return $state.href('archive.list');
+      return $state.href('archive.' + (id ? 'view' : 'list'), {
+        id: id,
+      });
     }
+  }
 })();
--- admin/app/archive/list/list.routes.js
+++ admin/app/archive/list/list.routes.js
@@ -1,23 +1,23 @@
 (function () {
-    angular
-        .module('pkg.backup.archive.list')
-        .config(routeConfig)
-    ;
+  angular
+    .module('pkg.backup.archive.list')
+    .config(routeConfig)
+  ;
 
-    /**
-     * @ngInject
-     */
-    function routeConfig(RouteHelpersProvider) {
-        var helper = RouteHelpersProvider;
-        var pkg = helper.package('backup');
-        pkg
-            .state('archive.list', {
-                url: '',
-                title: 'Archives',
-                controller: 'PkgBackupArchiveIndexCtrl as vm',
-                templateUrl: pkg.asset('admin/archive/list/list.index.html'),
-            })
-        ;
-    }
+  /**
+   * @ngInject
+   */
+  function routeConfig(RouteHelpersProvider) {
+    var helper = RouteHelpersProvider;
+    var pkg = helper.package('backup');
+    pkg
+      .state('archive.list', {
+        url: '',
+        title: 'Archives',
+        controller: 'PkgBackupArchiveIndexCtrl as vm',
+        templateUrl: pkg.asset('admin/archive/list/list.index.html'),
+      })
+    ;
+  }
 
 })();
